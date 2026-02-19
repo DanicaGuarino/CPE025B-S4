5d49568bd3b6204c4c15756c76b861b9083a9a9f
DanicaGuarino <danicaguarino@gmail.com>
2026-02-19 19:11:13 +0800

    HOA 5.1

--- JAVASCRIPT ESSENTIALS/Module_1/Procedure 1.js
+++ JAVASCRIPT ESSENTIALS/Module_1/Procedure 1.js
@@ -5,7 +5,7 @@ let name1 = "Maxwell Wright";
 let phone1 = "(0191) 719 6495";
 let email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
 
-// --- C2nd Contact: Raja Villarreal ---
+// --- 2nd Contact: Raja Villarreal ---
 let name2 = "Raja Villarreal";
 let phone2 = "0866 398 2895";
 let email2 = "posuere.vulputate@sed.com";

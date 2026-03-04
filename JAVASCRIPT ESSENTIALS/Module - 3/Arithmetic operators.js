28577b78e74a83e211851faf7a3c51f41312ca90
DanicaGuarino <danicaguarino@gmail.com>
2026-03-04 20:44:27 +0800

    Seatwork 6.1

--- JAVASCRIPT ESSENTIALS/Module - 3/Arithmetic operators.js
+++ JAVASCRIPT ESSENTIALS/Module - 3/Arithmetic operators.js
@@ -1,3 +1,4 @@
+// Arithmetic operators are used to perform mathematical operations on numbers and return a numerical result.
 console.log(2 * 3 + 1);
 console.log(2 ** 4);
 console.log(5 * 1); 
--- JAVASCRIPT ESSENTIALS/Module - 3/Logical operators.js
+++ JAVASCRIPT ESSENTIALS/Module - 3/Logical operators.js
@@ -1,3 +1,4 @@
+// Logical operators are used to combine multiple boolean expressions and return a boolean result based on the logic of the operators.
 console.log(true || false);  
 console.log(false || ! false);  
 console.log(false || false || true);  
